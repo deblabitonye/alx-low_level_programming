2a2d3b18389c4cb691e5c685375a35de1d6c5c25
deblabitonye <bitonyedebra@gmail.com>
2023-09-09 05:21:54 -0700

    6-size.c

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -2,15 +2,15 @@
 /**
  * main - size of various types
  *
- * Return: 0 on success
+ * Return: 0 (success)
  */
 int main(void)
 {
-char 'a';
-int 'b';
-long int 'c';
-long long 'd';
-float 'f';
+char a;
+int b;
+long int c;
+long long d;
+float f;
 printf("Size of a char: %lu byte(s)\n", (unsigned long)sizeof(a));
 printf("Size of an int: %lu byte(s)\n", (unsigned long)sizeof(b));
 printf("Size of a long int: %lu byte(s)\n" (unsigned long)sizeof(c));
