1b12035b40ba1bbe6fa4eec5aa4ae3489e8aa5e8
deblabitonye <bitonyedebra@gmail.com>
2023-09-09 02:55:34 -0700

    0-positive_or_negative.c

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -13,8 +13,8 @@ n = rand() - RAND_MAX / 2;
 if (n > 0)
 printf("%d is positive\n", n);
 else if (n == 0)
-printf("%d is 0\n", n);
+printf("%d is zero\n", n);
 else
-printf("%d is n\n", n);
+printf("%d is negative\n", n);
 return (0);
 }
