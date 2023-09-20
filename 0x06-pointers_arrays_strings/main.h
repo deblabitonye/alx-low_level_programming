4c6b2c8ec8207b0530b1bbe6bcc41a7271d28392
deblabitonye <bitonyedebra@gmail.com>
2023-09-20 08:41:33 -0700

    0-strcat.c

--- 0x06-pointers_arrays_strings/main.h
+++ 0x06-pointers_arrays_strings/main.h
@@ -1,8 +1,7 @@
 #ifndef MAIN_H
 #define MAIN_H
-
 int _putchar(char c);
-char *_strcat(char *dest, char *src)
+char *_strcat(char *dest, char *src);
 char *_strncat(char *dest, char *src, int n);
 char *_strncpy(char *dest, char *src, int n);
 int _strcmp(char *s1, char *s2);
@@ -11,4 +10,3 @@ char *string_toupper(char *);
 char *cap_string(char *);
 char *leet(char *);
 #endif
-
