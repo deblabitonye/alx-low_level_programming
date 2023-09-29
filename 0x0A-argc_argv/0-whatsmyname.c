7ac91d9dd878d7b02efa6e07ca1523c82a7a9dc6
deblabitonye <bitonyedebra@gmail.com>
2023-09-29 12:40:39 -0700

    0-whatsmyname.c

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,12 @@
+#include "main.h"
+/**
+ * main - prints programme name
+ * @argc: number of arguments
+ * @argv: array of arguments
+ * Return: always 0 (success)
+ */
+int main(int argc __attribute__((unused)), char *argv[])
+{
+printf("%s\n", *argv);
+return (0);
+}
