27cb718f619f10f28e8fd9b95da054bf10e4186e
deblabitonye <bitonyedebra@gmail.com>
2023-10-10 02:34:06 -0700

    dog.h

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -1,22 +1,18 @@
-#ifndef DOG_H
-#define DOG_H
+#ifndef _DOG_H_
+#define _DOG_H_
+
 /**
  * struct dog - a dog's basic info
  * @name: name to initialize
  * @age: age to initialize
  * @owner: owner to initialize
+ *
+ * Description: longer description
  */
 struct dog
 {
 char *name;
 float age;
 char *owner
-}
-void init_dog(struct dog *d, char *name, float age, char *owner);
-void print_dog(struct dog *d);
-typedef struct dog dog_t;
-dog_t *new_dog(char *name, float age, char *owner);
-void free_dog(dog_t *d);
-char *_strcpy(char *dest, char *src);
-int _strlen(char *s);
+};
 #endif
