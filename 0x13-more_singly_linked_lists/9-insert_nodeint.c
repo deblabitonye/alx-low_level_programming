39964b243277329bd167ba94a30f33b671c4e2bd
deblabitonye <bitonyedebra@gmail.com>
2023-10-24 03:36:53 -0700

    9-insert_nodeint.c

--- 0x13-more_singly_linked_lists/9-insert_nodeint.c
+++ 0x13-more_singly_linked_lists/9-insert_nodeint.c
@@ -8,7 +8,7 @@
 listint_t *create_new_node(int n)
 {
 listint_t *new_node;
-new_node = malloc(sizeof(listing_t));
+new_node = malloc(sizeof(listint_t));
 if (new_node == NULL)
 return (NULL);
 new_node->n = n;
@@ -26,7 +26,7 @@ listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
 {
 unsigned int i;
 listint_t *new_node;
-listint_t *tmp
+listint_t *tmp;
 listint_t *tmp_old;
 tmp = *head;
 if (head == NULL)
