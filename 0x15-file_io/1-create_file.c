de656b014108ebadee8aec571d38f5ebb88ee082
deblabitonye <bitonyedebra@gmail.com>
2023-10-30 04:45:53 -0700

    1-create_file.c

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -14,7 +14,7 @@ int bletters;
 if (!filename)
 return (-1);
 
-file_a = open(filename, O_CREATE | O_WRONLY | O_TRUNC, 0600);
+file_a = open(filename, O_CREAT | O_WRONLY | O_TRUNC, 0600);
 if (file_a == -1)
 return (-1);
 if (!text_content)
@@ -26,4 +26,4 @@ if (rwr == -1)
 return (-1);
 close(file_a);
 return (1);
-
+}
