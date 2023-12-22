8143fbbd1ffaa551f06c791e0f66ac182d2a374c
deblabitonye <bitonyedebra@gmail.com>
2023-12-22 03:37:35 -0800

    3-hash_table_set.c

--- 0x1A-hash_tables/3-hash_table_set.c
+++ 0x1A-hash_tables/3-hash_table_set.c
@@ -10,70 +10,36 @@
  */
 int hash_table_set(hash_table_t *ht, const char *key, const char *value)
 {
-unsigned long int index, i;
-hash_node_t *new;
-char *value_copy;
+unsigned long int index;
+hash_node_t *hash_node, *temp;
+char *new_value;
 
-if (ht == NULL || key == NULL || *key == '\0' || value == NULL)
-return 0;
-
-value_copy = strdup(value);
-if (value_copy == NULL)
+if (ht == NULL || ht->array == NULL || ht->size == 0 ||
+key == NULL || strlen(key) == 0 || value == NULL)
 return (0);
-    
 index = key_index((const unsigned char *)key, ht->size);
-for (i = index; ht->array[i]; i++)
+temp = ht->array[index];
+while (temp != NULL)
 {
-if (strcmp(ht->array[i]->key, key) == 0)
+
+if (strcmp(temp->key, key) == 0)
 {
-free(ht->array[i]->value);
-ht->array[i]->value = value_copy;
+
+new_value = strdup(value);
+if (new_value == NULL)
+return (0);
+free(temp->value);
+temp->value = new_value;
 return (1);
+
 }
+temp = temp->next;
+
 }
-
-new = malloc(sizeof(hash_node_t))
-if (new == NULL)
-
-
-    /* Check if the key already exists in the hash table */
-    temp = ht->array[index];
-    while (temp != NULL)
-    {
-        /* If the key already exists, update the value and return 1 */
-        if (strcmp(temp->key, key) == 0)
-        {
-            free(temp->value);
-            temp->value = strdup(value);
-            if (temp->value == NULL)
-                return 0;
-            return 1;
-        }
-        temp = temp->next;
-    }
-
-    /* If the key doesn't exist, create a new node and add it to the list */
-    new_node = malloc(sizeof(hash_node_t));
-    if (new_node == NULL)
-        return 0;
-
-    new_node->key = strdup(key);
-    if (new_node->key == NULL)
-    {
-        free(new_node);
-        return 0;
-    }
-
-    new_node->value = strdup(value);
-    if (new_node->value == NULL)
-    {
-        free(new_node->key);
-        free(new_node);
-        return 0;
-    }
-
-    new_node->next = ht->array[index];
-    ht->array[index] = new_node;
-
-    return 1;
+hash_node_t = make_hash_node_t(key, value);
+if (hash_node == NULL)
+return (0);
+hash_node->next = ht->array[index];
+ht->array[index] = hash_node;
+return (1);
 }
